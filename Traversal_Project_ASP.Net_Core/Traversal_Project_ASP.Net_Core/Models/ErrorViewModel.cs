30730115a6b4dbe6d6de372039fb5e6683525596
Ertugrul <57365047+officialerto@users.noreply.github.com>
2023-04-12 23:46:29 +0300

    Traversal_Project_ASP.Net_Core
    Create Project and Add Template to Website

---
+++ Traversal_Project_ASP.Net_Core/Traversal_Project_ASP.Net_Core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Traversal_Project_ASP.Net_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

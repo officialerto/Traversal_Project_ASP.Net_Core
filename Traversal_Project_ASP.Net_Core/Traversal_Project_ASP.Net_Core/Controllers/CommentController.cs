74b6ff0268baedec059711b9157a4837e7b0aa54
Ertugrul <57365047+officialerto@users.noreply.github.com>
2023-05-22 23:22:55 +0300

    TraversalProject_LastUpdate_22.05.23
    5.part

---
+++ Traversal_Project_ASP.Net_Core/Traversal_Project_ASP.Net_Core/Controllers/CommentController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+using DataAccessLibrary.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Traversal_Project_ASP.Net_Core.Controllers
+{
+    public class CommentController : Controller
+    {
+        CommentManager commentManager = new CommentManager(new EfCommentDal());
+
+        [HttpGet]
+        public PartialViewResult AddComment()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult AddComment(Comment p)
+        {
+            p.CommentDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            p.CommentState = true;
+            commentManager.TAdd(p);
+            return PartialView();
+        }
+    }
+}
